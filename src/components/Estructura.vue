b52d9696b6641072f90f1c8c2e271927fbeb83ff
jmartinezbalkarran <jmartinezbalkarran@gmail.com>
2025-10-28 23:35:09 -0400

    form

--- src/components/Estructura.vue
+++ src/components/Estructura.vue
@@ -184,7 +184,7 @@
             });
         }
         document.getElementById('btnFlotante').     addEventListener('click', () => {
-  window.location.href = '';
+  window.location.href = '/form';
 });
         loadDataAndRender();
     });
